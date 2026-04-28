9f94c4db03c7adfde84b48608ca5136194bce6bc
Theo Midrouillet <theomidrouillet@gmail.com>
2026-04-28 09:49:58 +0200

    fixed rf

--- random_forest/random_forest_regressor.py
+++ random_forest/random_forest_regressor.py
@@ -2,7 +2,6 @@ import os
 
 import pandas as pd
 import numpy as np
-from shap import kmeans
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
